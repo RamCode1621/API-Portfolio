17836a262fd7c8cc8a8b19bf76fcb35e338b3fd1
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2021-12-11 18:55:31 -0500

    v1.0.2

--- src/routes/portfolio.routes.js
+++ src/routes/portfolio.routes.js
@@ -2,6 +2,11 @@ const express=require('express')
 const router=express()
 const {getProjects,getProjectDetails,getMyInfo,saveData,saveMyInfo}=require('../controllers/routes.controllers')
 
+router.get('/',(req,res)=>{
+    res.json({
+        Autor:'Richard Aguilar'
+    })
+}) 
 router.get('/projects',getProjects) 
 router.get('/project/:name',getProjectDetails)
 router.get('/info',getMyInfo)
