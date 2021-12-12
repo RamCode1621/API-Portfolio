a6504f7ff291d65c02cc7fcc85d9c44d1a575f74
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2021-12-11 22:02:50 -0500

    v1.0.7

--- src/routes/portfolio.routes.js
+++ src/routes/portfolio.routes.js
@@ -2,19 +2,13 @@ const express=require('express')
 const router=express()
 const {getProjects,getProjectDetails,getMyInfo,saveData,saveMyInfo}=require('../controllers/routes.controllers')
 
-const ModelProject=require('../models/Project')
-const ModelMyInfo=require('../models/MyInfo')
+// const ModelProject=require('../models/Project')
+// const ModelMyInfo=require('../models/MyInfo')
 
 router.get('/',(req,res)=>{
     res.redirect('/info')
 }) 
-router.get('/projects',(req,res)=>{
-    ModelProject.find()
-    .then(data=>res.json(data)) 
-    .catch(error=>res.json({  
-        database:'Not find'
-    }))
-}) 
+router.get('/projects',getProjects) 
 
 router.get('/project/:name',getProjectDetails)
 router.get('/info',getMyInfo)
