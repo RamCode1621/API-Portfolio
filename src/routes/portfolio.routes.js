deb999a9b6a5974701ccfd065e088ae51a7d9341
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2021-12-16 21:00:04 -0500

    v1.0.13

--- src/routes/portfolio.routes.js
+++ src/routes/portfolio.routes.js
@@ -1,21 +1,21 @@
 //Routes
 
 const express=require('express')
-const router=express.Router()
+const route=express()
 const {getProjects,getProjectDetails,getMyInfo,saveData,saveMyInfo}=require('../controllers/routes.controllers')
 
 // const ModelProject=require('../models/Project')
 // const ModelMyInfo=require('../models/MyInfo')
 
-router.get('/',(req,res)=>{
+route.get('/',(req,res)=>{
     res.send('My API!!!!!')
 })
 
-router.get('/projects',getProjects) 
-router.get('/project/:name',getProjectDetails)
-router.get('/info',getMyInfo)
+route.get('/projects',getProjects) 
+route.get('/project/:name',getProjectDetails)
+route.get('/info',getMyInfo)
 
 // router.get('/save-data',saveData)
 // router.get('/save-myInfo',saveMyInfo)
 
-module.exports=router   
+module.exports=route 
