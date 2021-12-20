eb0d06d2c6b47a929fb8b5cb46c74628c80137cd
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2021-12-20 11:39:52 -0500

    v1.0.24

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config('./')
 const app=require('./src/server')
 require('./src/database')
 
-const port=process.env.PORT
+const port=process.env.PORT|| 4000
  
 app.listen(port,(req,res)=>{ 
     console.log(`Server on port ${port}`)
