73521b36fffa9000d70d0911cd3aad808ecc5a4b
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2022-01-10 23:33:49 -0500

    Edit access control allow origin

--- src/server.js
+++ src/server.js
@@ -7,14 +7,13 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "https://richardaguilar.info/");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 })
  
 app.use(require('./routes/portfolio.routes'))
-// app.use(require('./routes/users.routes'))
 
 app.use(express.static(__dirname+'/../public'))
 
