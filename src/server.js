c3718e6a1940dda2915b7fe0cc8401202583a0bb
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2022-01-14 22:23:29 -0500

    all acces to cors origin

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "https://richardaguilar.info/");
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
